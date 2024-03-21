feccc12ea66e43ecb59154f4a18c0180b5bd90d4
Thanh-Tung Le <thanhtul@simons-2.ics.uci.edu>
2024-03-21 09:12:28 -0700

    implemented ground field

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
-CUDA_VISIBLE_DEVICES=0 python3 -m scripts.train_field field=hybrid_grid dataset=image field.d_grid_feature=128 field.grid.side_length=33 field.mlp.positional_encoding_octaves=null field.mlp.num_hidden_layers=2 field.mlp.d_hidden=64
+CUDA_VISIBLE_DEVICES=0 python3 -m scripts.train_field field=ground_plan \
+                                    dataset=implicit \
+                                    dataset.function=torus \
+                                    field.d_grid_feature=128 \
+                                    field.positional_encoding_octaves=8 \
+                                    field.grid.side_length=65 \
+                                    field.mlp.positional_encoding_octaves=null \
+                                    field.mlp.num_hidden_layers=2 \
+                                    field.mlp.d_hidden=64 \
+
+# CUDA_VISIBLE_DEVICES=0 python3 -m scripts.train_field field=hybrid_grid dataset=image field.d_grid_feature=128 field.grid.side_length=33 field.mlp.positional_encoding_octaves=null field.mlp.num_hidden_layers=2 field.mlp.d_hidden=64
 # CUDA_VISIBLE_DEVICES=0 python3 -m scripts.train_field field=grid dataset=image field.side_length=33
 # CUDA_VISIBLE_DEVICES=0 python3 -m scripts.train_field field=siren dataset=image learning_rate=1e-4
 # CUDA_VISIBLE_DEVICES=0 python3 -m scripts.compare_fields
--- src/field/field_ground_plan.py
+++ src/field/field_ground_plan.py
@@ -1,8 +1,12 @@
 from jaxtyping import Float
 from omegaconf import DictConfig
 from torch import Tensor
+import torch
 
 from .field import Field
+from .field_grid import FieldGrid
+from .field_mlp import FieldMLP
+from ..components import PositionalEncoding
 
 
 class FieldGroundPlan(Field):
@@ -22,7 +26,24 @@ class FieldGroundPlan(Field):
         """
         super().__init__(cfg, d_coordinate, d_out)
         assert d_coordinate == 3
-        raise NotImplementedError("This is your homework.")
+        
+        #positional encoding for z
+        self.z_pos_embedding = PositionalEncoding(cfg.positional_encoding_octaves)
+        zd_coordinate = PositionalEncoding(cfg.positional_encoding_octaves).d_out(1)
+        
+        #grid using x and y coordinates
+        self.grid_field = FieldGrid(
+            cfg.grid,
+            2,
+            cfg.d_grid_feature,
+        )
+        
+        #concatenate the grid's outputs with the corresponding encoded Z values, then feed the result through the MLP
+        self.mlp_field = FieldMLP(
+            cfg.mlp,
+            cfg.d_grid_feature + zd_coordinate,
+            d_out
+        )
 
     def forward(
         self,
@@ -35,5 +56,18 @@ class FieldGroundPlan(Field):
         - Concatenate the grid's outputs with the corresponding encoded Z values, then
           feed the result through the MLP.
         """
-
-        raise NotImplementedError("This is your homework.")
+        
+        #get coordinates
+        z_coords = coordinates[:,-1] # N,
+        z_coords = z_coords.unsqueeze(-1) # Nx1
+        xy_coords = coordinates[:,:-1] # Nx2
+        
+        # Get the grid's outputs using the XY coordinates and the Z coordinates
+        xy_grid_field = self.grid_field(xy_coords) # Nx d_grid_feature
+        z_pos_embedding = self.z_pos_embedding(z_coords) # Nx zd_coordinate
+        
+        #concatenate the grid's outputs with the corresponding encoded Z values
+        concat_field = torch.cat((xy_grid_field, z_pos_embedding), dim=-1) # Nx (d_grid_feature + zd_coordinate)
+        out_mlp_field = self.mlp_field(concat_field)
+        
+        return out_mlp_field
